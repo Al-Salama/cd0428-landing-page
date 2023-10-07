32b554258178db0aef72ce7c8eef0d2752c9d783
Altamimi <77057341+Al-Salama@users.noreply.github.com>
2023-10-07 22:57:10 +0300

    feat: Add more functionalities and features
    * Add functionality to distinguish the section in view.
    * Add the functionality to scroll to sections by clicking on the navigation item.
    * Add an active state to the navigation items when a section is in the viewport.
    * Update the design of the page.
    * The page is now responsive for all devices.
    * Update the README file with the specific information of this project.

--- README.md
+++ README.md
@@ -1,9 +1,37 @@
 # Landing Page Project
 
-## Instructions
+Welcome to the Landing Page Project! This is a simple web project created using HTML, CSS, and JavaScript that features a multi-section landing page with a dynamically updating navigation menu. The navigation menu contains buttons that allow users to navigate to specific sections of the page, and both the navigation buttons and the sections are highlighted as the user scrolls through the content.
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+## Project Description
 
-To get started, open `js/app.js` and start building out the app's functionality
+This project is designed to showcase a responsive and interactive landing page with the following features:
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+- **Dynamic Navigation Menu:** The navigation menu is dynamically generated based on the sections in the HTML. It automatically updates to include buttons for each section, making it easy for users to jump to their desired content.
+
+- **Section Highlighting:** As the user scrolls down the page, the current section being viewed is highlighted in both the navigation menu and the page content. This provides a visual indicator of the user's current location on the page.
+
+- **Ease of Use:** The project is easy to set up and use, making it a great starting point for creating similar landing pages or interactive websites.
+
+## Usage
+
+Follow these simple instructions to download and run the project:
+
+### Downloading the Project
+
+1. Open your terminal or command prompt.
+2. Navigate to the directory where you want to clone the project using the `cd` command:
+   ```bash
+    cd /path/to/your/desired/directory
+   ```
+3. Clone the project repository using the following Git command:
+   ```bash
+    git clone https://github.com/your-username/multi-section-landing-page.git
+   ```
+4. Once the cloning process is complete, navigate to the project directory:
+   ```bash
+    cd multi-section-landing-page
+   ```
+
+### Running the Project
+1. After downloading the project, simply open the `index.html` file in your web browser. You can do this by double-clicking the file or right-clicking and selecting "Open with" your preferred browser.
+2. The landing page should now be displayed in your web browser, and you can start exploring its features. Scroll down to see the dynamic navigation menu and section highlighting in action.
--- js/app.js
+++ js/app.js
@@ -22,30 +22,84 @@
  * Define Global Variables
  *
  */
-const nav = document.querySelector("nav.navbar__menu");
-const navList = nav.querySelector("ul");
+const ACTIVE_SEC_CLASS_NAME = "sec-active";
+const ACTIVE_ITEM_CLASS_NAME = "item-active";
 
-let sections;
+let nav = HTMLElement.prototype;
+let navList = HTMLUListElement.prototype;
+
+let sections = [HTMLElement.prototype];
+const navItems = [
+  { li: HTMLLIElement.prototype, a: HTMLAnchorElement.prototype },
+];
+
+let currentIntersection = {
+  secId: "",
+  ratio: 0,
+};
+let scrollingIntoSection = false;
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
  */
-function storeSectionsData() {
+
+/**
+* @description Gets section elements
+* @returns {HTMLElement[] | undefined} An array of HTMLElement or undefined if there are no sections in the DOM.
+*/
+function getSectionsData() {
   const sectionsElms = document.querySelectorAll("main section");
   if (!sectionsElms || sectionsElms.length === 0) return;
 
   const data = [];
   for (const secElm of sectionsElms) {
-    data.push({
-      id: secElm.id,
-      navName: secElm.dataset.nav,
-      isActive: secElm.classList.contains("sec-active")
-    });
+    if (secElm instanceof HTMLElement) {
+      // to tell javascript that this is an HTMLElement.
+      data.push(secElm);
+    }
   }
 
   return data;
+}
+
+/**
+* @description Sets a section to be an active section.
+* @param {string} activeSectionId - The section id.
+* @returns {void}
+*/
+function setActiveSection(activeSectionId) {
+  for (const sec of sections) {
+    if (sec.id === activeSectionId) {
+      sec.classList.add(ACTIVE_SEC_CLASS_NAME);
+      /*
+        Do not scroll to the closest section while the user
+        is navigating to another section using the Navigation Menu.
+      */
+      if (!scrollingIntoSection) scrollToSectionView(sec.id);
+    } else sec.classList.remove(ACTIVE_SEC_CLASS_NAME);
+  }
+
+  for (const navItem of navItems) {
+    if (navItem.a.href.split("#")[1] === activeSectionId) {
+      navItem.li.classList.add(ACTIVE_ITEM_CLASS_NAME);
+    } else navItem.li.classList.remove(ACTIVE_ITEM_CLASS_NAME);
+  }
+}
+
+/**
+* @description Perform a smooth scrolling to a specific section.
+* @param {string} sectionId - The section id.
+* @returns {HTMLElement[] | undefined} An array of HTMLElement or undefined if there are no sections in the DOM.
+*/
+function scrollToSectionView(sectionId) {
+  const sec = sections.find((value) => value.id === sectionId);
+  if (sec) scrollingIntoSection = true;
+  sec.scrollIntoView({
+    behavior: "smooth",
+    block: "center",
+  });
 }
 
 /**
@@ -54,36 +108,93 @@ function storeSectionsData() {
  *
  */
 
-// build the nav
+// build the nav using DocumentFragment and setup our events.
 function buildNav() {
-  sections = storeSectionsData();
+  sections = getSectionsData();
   if (!sections) return;
+
+  nav = document.querySelector("nav.navbar__menu");
+  navList = nav.querySelector("ul.navbar__list");
+
+  // remove the initial value.
+  navItems.splice(0, navItems.length);
 
   const fragment = document.createDocumentFragment();
   for (const sec of sections) {
-    const navLi = document.createElement("li");
+    const navItem = document.createElement("li");
     const navAnchor = document.createElement("a");
 
-    navAnchor.textContent = sec.navName;
+    navAnchor.classList.add("menu__link");
     navAnchor.href = `#${sec.id}`;
+    navAnchor.textContent = sec.dataset.nav;
 
-    // I added this data on top of the href because i want to use
-    // .getElementById without using string manipulations.
-    navAnchor.dataset.scrollTo = sec.id;
+    if (sec.classList.contains(ACTIVE_SEC_CLASS_NAME))
+      navItem.classList.add(ACTIVE_ITEM_CLASS_NAME);
 
-    if (sec.isActive) navLi.classList.add("nav-active");
+    navItems.push({
+      li: navItem,
+      a: navAnchor,
+    });
 
-    navLi.appendChild(navAnchor);
-    fragment.appendChild(navLi);
+    navItem.appendChild(navAnchor);
+    fragment.appendChild(navItem);
   }
 
   navList.appendChild(fragment);
+  setupEvents();
 }
-buildNav();
 
-// Add class 'active' to section when near top of viewport
+// The callback function that will be called when there is a change of intersection states or ratio.
+function sectionObserverCallback(
+  entries = [IntersectionObserverEntry.prototype]
+) {
+  const intersectingSec = {
+    secId: "",
+    ratio: 0,
+  };
 
-// Scroll to anchor ID using scrollTO event
+  let removeCurrentIntersection = false;
+  for (const entry of entries) {
+    if (entry.target.id === currentIntersection.secId) {
+      // if this entry is the current active section.
+
+      if (!entry.isIntersecting) {
+        // if it's not intersecting anymore, then set it to be removed if there is no other section is intersecting.
+        removeCurrentIntersection = true;
+      } else {
+        // otherwise, update its intersection ratio.
+        currentIntersection.ratio = entry.intersectionRatio;
+      }
+    } else {
+      // skip this entry if it's not intersecting.
+      if (!entry.isIntersecting) continue;
+
+      // Compare with the current section intersection ratio and select the beggest ratio.
+      if (entry.intersectionRatio > currentIntersection.ratio) {
+        if (entry.intersectionRatio > intersectingSec.ratio) {
+          intersectingSec.secId = entry.target.id;
+          intersectingSec.ratio = entry.intersectionRatio;
+        }
+      }
+    }
+  }
+  if (removeCurrentIntersection && intersectingSec.ratio === 0) {
+    /*
+    If the current section is set to be remove and there is no other
+    sections that is intersecting, then remove the current active section.
+    */
+    currentIntersection.secId = "";
+    currentIntersection.ratio = 0;
+    setActiveSection("");
+  } else if (intersectingSec.secId !== currentIntersection.secId && intersectingSec.ratio > 0) {
+    /*
+    Otherwise, check if there is new section
+    that is intersecting and set it to be active.
+    */
+    currentIntersection = intersectingSec;
+    setActiveSection(currentIntersection.secId);
+  }
+}
 
 /**
  * End Main Functions
@@ -92,7 +203,42 @@ buildNav();
  */
 
 // Build menu
+document.addEventListener("DOMContentLoaded", buildNav);
 
-// Scroll to section on link click
+function setupEvents() {
+  // Scroll to section on link click
+  navList.addEventListener("click", (ev) => {
+    const target = ev.target;
+    if (target instanceof HTMLAnchorElement) {
+      // Might be a wierd check, but this will give my variable auto-completion.
 
-// Set sections as active
+      ev.preventDefault();
+      scrollToSectionView(target.href.split("#")[1]);
+    }
+  });
+
+  /*
+  Setup the Intersection Observer of our sections so that we
+  are aware ofwhich section the user is intersecting with.
+  */
+  const threshold = [];
+  for (let index = 7; index <= 20; index++) {
+    // Thresh holds starts from 7*0.05 = 0.35, where it is the minimum ratio for the section to be considered intersecting.
+    threshold.push(index * 0.05);
+  }
+  // Setup intersection observer to observe the sections.
+  // See: https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver
+  const sectionObserver = new IntersectionObserver(sectionObserverCallback, {
+    threshold,
+  });
+
+  for (const sec of sections) {
+    sectionObserver.observe(sec);
+  }
+
+  // Set the variable 'scrollingIntoSection' to false when the user stops scrolling.
+  // 'scrollingIntoSection' is true when the user clicks on a navigation item, e.g: Section 3.
+  document.onscrollend = (ev) => {
+    if (scrollingIntoSection) scrollingIntoSection = false;
+  };
+}
